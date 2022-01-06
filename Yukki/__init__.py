ba31cd81c2eb02391fe27988dc48252d31fba311
King Amda <86409667+King-Amda@users.noreply.github.com>
2022-01-06 17:05:45 +0530

    Update __init__.py

--- Yukki/__init__.py
+++ Yukki/__init__.py
@@ -67,13 +67,13 @@ async def initiate_bot():
     )
     console.print(header)
     with console.status(
-        "[magenta] Booting up The Yukki Music Bot...",
+        "[magenta] Booting up The Nipun Music Bot...",
     ) as status:
         console.print("┌ [red]Booting Up The Clients...\n")
         await app.start()
         await userbot.start()
         console.print("└ [green]Clients Booted Successfully!")
-        initial = await startup_send_new("Starting Yukki Music Bot...")
+        initial = await startup_send_new("Starting Nipun Music Bot...")
         await asyncio.sleep(0.5)
         all_over = await startup_send_new("Checking Required Directories...")
         console.print(
